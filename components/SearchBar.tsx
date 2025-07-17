78956cb59e183b2314e9b315d61914258abd647e
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-07-17 17:07:49 +0530

    Update: search page and functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,18 +4,20 @@ import { icons } from '@/constants/icons'
 
 interface props {
     placeholder: string;
-    onPress?: () => void
+    onPress?: () => void,
+    value?: string,
+    onChangeText?: (text: string) => void,
 }
 
-const SearchBar = ({ onPress, placeholder }: props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=''
-                onChangeText={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 className='flex-1 ml-2 text-white'
                 placeholderTextColor='#a8b5db'
             />
